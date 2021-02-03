0fd3c30098a5cf42594d14c065a927a0d65297a1
Stanly0407 <Shelestova_S_V@mail.ru>
2021-02-03 18:33:48 +0300

    part 1

--- src/main/java/com/epam/task/third/parsing/DataValidator.java
+++ src/main/java/com/epam/task/third/parsing/DataValidator.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class DataValidator {
 
     public boolean validateLineContent(String line) {
-        String onlyDigitsAndSpaces = "^([\\d -.]\\s*)+$"; //Первая цифра д.б. положительной.
+        String onlyDigitsAndSpaces = "^([\\d -.]\\s*)+$";
         Pattern linePattern = Pattern.compile(onlyDigitsAndSpaces);
         Matcher matcher = linePattern.matcher(line);
         return matcher.matches();
